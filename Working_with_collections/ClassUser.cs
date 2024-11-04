26f6a2861e80514e2611875e0bf4dcfba751d459
Victoria <vika.200.viktoria@gmail.com>
2024-11-04 10:00:52 +0200

    Add project files.

---
+++ Working_with_collections/ClassUser.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Working_with_collections
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public User(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+        public override string ToString()
+        {
+            return $"Айді студента: {Id}, Ім'я: {Name}";
+        }
+    }
+    public class UserManager
+    {
+        private List<User> students = new List<User>();
+        // Додавання нового користувача до списку
+        public void AddTask(int Newid, string Newname)
+        {
+            User Newstudent = new User(Newid, Newname);
+            students.Add(Newstudent);
+            Console.WriteLine($"Доданий студент: {Newstudent}");
+        }
+        // Видалення студента за ідентифікатором
+        public void RemoveTask(int studId)
+        {
+            User taskToRemove = students.Find(x => x.Id == studId);
+            if (taskToRemove != null)
+            {
+                students.Remove(taskToRemove);
+                Console.WriteLine($"Видалений студент: {taskToRemove}");
+            }
+            else
+            {
+                Console.WriteLine($"Студент з ідентифікатором {studId} не знайдено.");
+            }
+        }
+        // Виведення на екран усіх студентів у списку
+        public void DisplayTasks()
+        {
+            Console.WriteLine("Список студентів:");
+            foreach (var student in students)
+            {
+                Console.WriteLine(student);
+            }
+        }
+        // Тестування
+        static void Main()
+        {
+            UserManager taskManager = new UserManager();
+
+            // Додаємо кілька студентів до списку
+            taskManager.AddTask(1, "Alex");
+
+            taskManager.AddTask(2, "Victoria");
+            taskManager.AddTask(3, "Kate");
+
+            taskManager.RemoveTask(1);
+            // Виводимо всіх студентів
+            taskManager.DisplayTasks();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Working_with_collections/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*namespace Working_with_collections
+{
+    public class Task
+    {
+        public int TaskId { get; set; }
+        public string Description { get; set; }
+        public Task (int taskId, string description)
+        {
+            TaskId = taskId;
+            Description = description;
+        }
+        public override string ToString()
+        {
+            return $"Task ID: {TaskId}, Description: {Description}";
+        }
+    }
+    public class TaskManager
+    {
+        private List<Task> tasks = new List <Task>();
+        // Додавання нового завдання до списку
+        public void AddTask (int Newid, string Newdesc)
+        {
+            Task Newtask = new Task(Newid, Newdesc);
+            tasks.Add(Newtask);
+            Console.WriteLine($"Task added: {Newtask}");
+        }
+        // Видалення завдання за ідентифікатором
+        public void RemoveTask(int taskId)
+        {
+            Task taskToRemove = tasks.Find(x => x.TaskId == taskId);
+            if (taskToRemove != null)
+            {
+                tasks.Remove(taskToRemove);
+                Console.WriteLine($"Видалене завдання: {taskToRemove}");
+            }
+            else
+            {
+                Console.WriteLine($"Завдання з ідентифікатором {taskId} не знайдено.");
+            }
+        }
+        // Виведення на екран усіх завдань у списку
+        public void DisplayTasks()
+        {
+            Console.WriteLine("Список:");
+            foreach (var task in tasks)
+            {
+                Console.WriteLine(task);
+            }
+        }
+        // Тестування
+            static void Main()
+            {
+                TaskManager taskManager = new TaskManager();
+               
+                // Додаємо кілька завдань до списку
+                taskManager.AddTask(1, "Виконати домашнє завдання");
+
+                taskManager.AddTask(2, "Поприбирати вдома");
+                taskManager.AddTask(3, "Приготувати вечерю");
+
+                taskManager.RemoveTask(1);
+                // Виводимо всі завдання
+                taskManager.DisplayTasks();
+
+                Console.ReadLine();
+            }
+        }
+    }
+*/
